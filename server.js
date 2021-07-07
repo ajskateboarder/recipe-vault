ea65e78363b5171318b019a311c909de520543a1
themysticsavages <82041621+ajskateboarder@users.noreply.github.com>
2021-07-06 17:00:50 -0700

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get('/', function(req, res){
-    res.send('hello world')
-})
-
-app.listen(3000)
-console.log('Server listening on port 3000!')
